083774d8524c6b738d3ab35c981839e54a670980
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-19 16:40:20 +0200

    Rafactor class Product

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace csharp_oop_shop
+﻿using static csharp_oop_shop.Program;
+
+namespace csharp_oop_shop
 {
     internal class Program
     {
@@ -8,8 +10,18 @@
             Product product1 = new Product();
             product1.Name = "Banana";
             product1.Description = "Una fruta";
-            product1.Price = 2.222M;
-            product1.Vat = "sDFSDfsd";
+            product1.Price = -100.00M;
+
+
+            product1.PrintBaseInformation();
+            product1.PrintPriceWithVat();
+            product1.PrintNameExtended();
+
+            Product product2 = new Product("Mela", "Una frutta", -200M, 10);
+
+            product2.PrintBaseInformation();
+            product2.PrintPriceWithVat();
+            product2.PrintNameExtended();
 
         }
 
@@ -18,22 +30,101 @@
             private int productCode;
             public string Name {  get; set; }
             public string Description { get; set; }
-            public decimal Price { get; set; }
-            public string Vat { get; set; }
 
+            public decimal price;
+
+            private int vat;
+
+            // -----------
             // Costruttore
+            // -----------
             public Product()
             {
                 Random rnd = new Random();
                 this.productCode = rnd.Next(1000);
+                this.Vat = 22;
+            }
+            public Product(string name, string desctiption, decimal price, int vat)
+            {
+                Random rnd = new Random();
+                this.productCode = rnd.Next(1000);
+                this.Name = name;
+                this.Description = desctiption;
+                this.price = Math.Abs(price);
+                this.Vat = vat;
             }
 
+            // ---------
             // Get e Set
+            // ----------
             public int ProductCode
             {
                 get { return productCode; }
             }
 
+            public decimal Price
+            {
+                get { return price; }
+                set { price = Math.Abs(value); }
+            }
+
+            public int Vat
+            {
+                get { return vat; }
+                set { vat = SetVat(value); }
+            }
+
+            public decimal GetPriceWithVat()
+            {
+                decimal result = (this.price * ((decimal)this.Vat / 100 )) + this.price;
+
+                return decimal.Parse(result.ToString("0.00"));
+            }
+
+            public string GetNameExtended()
+            {
+                string result = this.productCode + this.Name;
+
+                return result;
+            }
+
+            // --------
+            // Funzioni
+            // --------
+
+            public void PrintBaseInformation()
+            {
+                Console.WriteLine();
+                Console.WriteLine(this.GetType().Name);
+                Console.WriteLine($"Code: {ProductCode}");
+                Console.WriteLine($"Name: {Name}");
+                Console.WriteLine($"Description: {Description}");
+                Console.WriteLine($"Price: {Price} euro");
+                Console.WriteLine($"Vat: {Vat} %");
+            }
+            
+            public void PrintPriceWithVat()
+            {
+                Console.WriteLine($"Price with vat: {GetPriceWithVat()} euro");      
+            }
+
+            public void PrintNameExtended()
+            {
+                Console.WriteLine($"Name extended: {GetNameExtended()}");
+            }
+
+            private int SetVat(int vat)
+            {
+                if (Math.Abs(vat) > 0 && Math.Abs(vat) < 100)
+                {
+                    return Math.Abs(vat);
+                }
+                else
+                {
+                    Console.WriteLine("Vat non è corretto");
+                    return 0;
+                }
+            }
         }
     }
 }
