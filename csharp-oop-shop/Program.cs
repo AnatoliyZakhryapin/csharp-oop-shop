960f3213b49cfa9cefda99a923d427a61c5fde22
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-19 14:47:47 +0200

    ADD Class Product

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+        }
+
+        public class Product
+        {
+            private int productCode;
+            public string Name {  get; set; }
+            public string Description { get; set; }
+            public decimal Price { get; set; }
+            public string Vat { get; set; }
+
+            // Costruttore
+            public Product()
+            {
+                Random rnd = new Random();
+                this.productCode = rnd.Next(1000);
+            }
+
+            // Get e Set
+            public int ProductCode
+            {
+                get { return productCode; }
+            }
+
         }
     }
 }
